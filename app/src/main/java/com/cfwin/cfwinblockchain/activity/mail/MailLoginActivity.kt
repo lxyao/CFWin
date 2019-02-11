bcb32e32f6355316d7f25dff2be479a8ad4e748d
lxyao <2278785476@qq.com>
2019-02-11 18:14:40 +0800

    调通pop3协议

--- app/src/main/java/com/cfwin/cfwinblockchain/activity/mail/MailLoginActivity.kt
+++ app/src/main/java/com/cfwin/cfwinblockchain/activity/mail/MailLoginActivity.kt
@@ -3,6 +3,7 @@ package com.cfwin.cfwinblockchain.activity.mail
 import android.content.Intent
 import android.text.TextUtils
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import butterknife.BindView
 import butterknife.OnClick
@@ -10,12 +11,8 @@ import com.cfwin.base.utils.PatternUtil
 import com.cfwin.cfwinblockchain.R
 import com.cfwin.cfwinblockchain.activity.SubBaseActivity
 import com.sun.mail.imap.IMAPFolder
-import com.sun.mail.imap.IMAPStore
-import java.security.NoSuchProviderException
 import java.security.Security
-import javax.mail.Folder
-import javax.mail.MessagingException
-import javax.mail.Session
+import javax.mail.*
 
 /**
  * 邮箱登陆界面
@@ -69,82 +66,84 @@ class MailLoginActivity: SubBaseActivity() {
             return
         }
         v?.isEnabled = false
-        //进行登陆
-        login()
+        object: Thread(){
+            override fun run() {
+                //进行登陆
+                login()
+            }
+        }.start()
     }
 
     private fun login(){
-
-        val host = "partner.outlook.cn"
-        val port = 993
-        val username = "leiyu@cfwin.com"
-        val password = "password"
-        val SSL_FACTORY = "javax.net.ssl.SSLSocketFactory"
+        startActivity(Intent(this, MsgListActivity::class.java))
+//        finish()
+        val host = "pop3.baisonmail.com"//"partner.outlook.cn"
+        val port = 995//993
+        val username = "dingyao.zhang@baisonmail.com"//"leiyu@cfwin.com"
+        val password = "baison@631"//""password"
+        val sslFactory = "javax.net.ssl.SSLSocketFactory"
         Security.addProvider(org.bouncycastle.jce.provider.BouncyCastleProvider())
-
         val props = System.getProperties()
-        props.put("mail.imap.port", "993")
-        props.put("mail.store.protocol","imap")
-        props.put("mail.imap.host", host)
-        // 如果设置了ssltls需要下面的设置
-        props.setProperty("mail.imap.socketFactory.class", SSL_FACTORY)
+//        props["mail.imap.port"] = "993"
+        props["mail.store.protocol"] = "pop3"//"imap"
+        props["mail.imap.host"] = host
+//        // 如果设置了ssltls需要下面的设置
+//        props.setProperty("mail.imap.socketFactory.class", sslFactory)
+        props.setProperty("mail.pop3.socketFactory.class", sslFactory)
         props.setProperty("mail.smtp.socketFactory.fallback", "false")
-        props.setProperty("mail.imap.socketFactory.port","993")
-        props.setProperty("mail.imap.port", "993")
-        props.put("mail.smtp.auth", "true")
-        props.setProperty("mail.imap.auth.login.disable", "true")
-        val session = Session.getInstance(props)
+//        props.setProperty("mail.imap.socketFactory.port","993")
+//        props.setProperty("mail.imap.port", "993")
+        props["mail.smtp.auth"] = "true"
+//        props.setProperty("mail.imap.auth.login.disable", "true")
+        props.setProperty("mail.pop3.auth.login.disable", "true")
+        val session = Session.getDefaultInstance(props, object: Authenticator(){
+            override fun getPasswordAuthentication(): PasswordAuthentication {
+                return PasswordAuthentication(username, password)
+            }
+        })
         session.debug = true
         var folder: IMAPFolder? = null
-        var store: IMAPStore? = null
+        var store: Store? = null
         try {
-            var erro = ""
-            try {
-                // 使用imap会话机制，连接服务器
-                store = session.getStore("imap") as IMAPStore?
-                store?.let {
-                    it.connect(username,password)
-                    //收件箱
-                    folder = it.getFolder("Sent Messages") as IMAPFolder?
-                    val defaultFolder: Folder = store.defaultFolder
-                    val allFolder = defaultFolder.list()
-                    for (i in allFolder.indices) {
-                        val str = allFolder[i].fullName
-                        System.out.println("这个是服务器中的文件夹=$str")
-                    }
-                    // 收件箱
-                    val folderInbox: IMAPFolder = store.getFolder("INBOX") as IMAPFolder
-                    folderInbox.open(Folder.READ_WRITE)
-                    // 使用只读方式打开收件箱
-                    //folder.open(Folder.READ_WRITE);
-                    val size = folderInbox.messageCount
-                    System.out.println("这里是打印的条数==$size")
-                    val mess=folderInbox.messages
-                    //  Message message = folder.getMessage(size)
-                    for (i in mess.indices) {
-                        val from = mess[i].from[0].toString();
-                        val subject = mess[i].subject
-                        val date = mess[i].sentDate
-                        System.out.println("From: $from")
-                        System.out.println("Subject: $subject")
-                        System.out.println("Date: $date")
-                    }
-                    /* String from = message.getFrom()[0].toString();
-             String subject = message.getSubject();
-             Date date = message.getSentDate();*/
-                    /* BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); */
-                    folder?.close(false)
-                    store.close()
+            // 使用imap会话机制，连接服务器
+//            store = session.getStore("imap") as IMAPStore?
+            store = session.getStore("pop3")
+            store?.let {
+                it.connect(host, port, username,password)
+                //收件箱
+                folder = it.getFolder("Sent Messages") as IMAPFolder?
+                val defaultFolder: Folder = store.defaultFolder
+                val allFolder = defaultFolder.list()
+                for (i in allFolder.indices) {
+                    val str = allFolder[i].fullName
+                    System.out.println("这个是服务器中的文件夹=$str")
                 }
-            }catch (e: Exception) {
-                erro = e.toString()
+                // 收件箱
+                val folderInbox: IMAPFolder = store.getFolder("INBOX") as IMAPFolder
+                folderInbox.open(Folder.READ_WRITE)
+                // 使用只读方式打开收件箱
+                //folder.open(Folder.READ_WRITE)
+                val size = folderInbox.messageCount
+                System.out.println("这里是打印的条数==$size")
+                val mess=folderInbox.messages
+                //  Message message = folder.getMessage(size)
+                for (i in mess.indices) {
+                    val from = mess[i].from[0].toString()
+                    val subject = mess[i].subject
+                    val date = mess[i].sentDate
+                    System.out.println("From: $from")
+                    System.out.println("Subject: $subject")
+                    System.out.println("Date: $date")
+                }
+                folder?.close(false)
+                store.close()
             }
-        } catch (e: NoSuchProviderException) {
-            e.printStackTrace();
-        } catch (e: MessagingException) {
-            e.printStackTrace();
+        }catch (e: MessagingException) {
+            e.printStackTrace()
+            runOnUiThread { findViewById<Button>(R.id.login).isEnabled = true }
+        }catch (e: Exception) {
+            e.printStackTrace()
+            runOnUiThread { findViewById<Button>(R.id.login).isEnabled = true }
         }
-//        startActivity(Intent(this, MsgListActivity::class.java))
-//        finish()
     }
 }
--- app/src/main/java/com/cfwin/cfwinblockchain/activity/mail/SendActivity.kt
+++ app/src/main/java/com/cfwin/cfwinblockchain/activity/mail/SendActivity.kt
@@ -2,13 +2,24 @@ package com.cfwin.cfwinblockchain.activity.mail
 
 import android.app.Activity
 import android.content.Intent
+import android.text.TextUtils
 import android.view.View
 import android.widget.*
 import butterknife.BindView
+import com.cfwin.base.utils.LogUtil
+import com.cfwin.cfwinblockchain.BuildConfig
 import com.cfwin.cfwinblockchain.R
 import com.cfwin.cfwinblockchain.activity.SubBaseActivity
 import com.cfwin.cfwinblockchain.activity.mail.contacts.ContactsActivity
 import com.cfwin.cfwinblockchain.beans.mail.ContactBean
+import java.security.Security
+import java.util.*
+import javax.mail.Message
+import javax.mail.MessagingException
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 /**
  * 邮件发送界面
@@ -30,6 +41,13 @@ class SendActivity: SubBaseActivity() {
     /**发送方式*/
     @BindView(R.id.sendMethod)lateinit var sendMethod: RadioGroup
 
+    private var secretType = R.id.cf
+    private lateinit var ts: Transport
+    private lateinit var message: MimeMessage
+    private var host = "smtp.sina.com"
+    private var loginUser = "liulzx234567890@sina.com"
+    private val loginPwd = "test"
+
     override fun getLayoutId(): Int {
         return R.layout.activity_send_mail
     }
@@ -40,12 +58,20 @@ class SendActivity: SubBaseActivity() {
     }
 
     override fun initData() {
+        if(!BuildConfig.DEBUG){
+            //获取配置信息
+        }
+        nickName.text = "用户"
+        mailAddress.text = loginUser
+        initSendInfo()
     }
 
     private fun onChecked(group: RadioGroup, checkedId: Int){
+        secretType = checkedId
         if(checkedId == R.id.pgp){
             showToast(getString(R.string.pgp_pubkey_hint))
             group.findViewById<RadioButton>(checkedId).isChecked = false
+            secretType = -1
         }
     }
 
@@ -58,9 +84,16 @@ class SendActivity: SubBaseActivity() {
             }
             R.id.toolbar_menu->{
                 //发送
+                checkSendMsg()
             }
+            R.id.iv_back-> onBackPressed()
             else-> super.onClick(v)
         }
+    }
+
+    override fun onBackPressed() {
+        ts.close()
+        super.onBackPressed()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -71,5 +104,88 @@ class SendActivity: SubBaseActivity() {
                 inUser.setText(user!!.mail)
             }
         }
+    }
+
+    private fun checkSendMsg(){
+        val inUser = inUser.text.toString().trim()
+        val subject = subject.text.toString().trim()
+        val content = content.text.toString().trim()
+        if(TextUtils.isEmpty(inUser)){
+            showToast(getString(R.string.inUser_hint))
+            return
+        }
+        if(TextUtils.isEmpty(subject)){
+            showToast(getString(R.string.subject_hint))
+            return
+        }
+        if(TextUtils.isEmpty(content)){
+            showToast(getString(R.string.send_content).replace("...", ""))
+            return
+        }
+        //指明邮件的发件人
+        message.setFrom(InternetAddress(loginUser))
+        //指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
+        message.setRecipient(Message.RecipientType.TO, InternetAddress(inUser))
+        //邮件的标题
+        message.subject = "test1 $subject"
+        object: Thread(){
+            override fun run() {
+                super.run()
+                sendMsg(content)
+            }
+        }.start()
+    }
+
+    private fun sendMsg(content: String){
+        var changeContent = ""
+        when(secretType){
+            R.id.none->{
+                //内容原样输出传递
+                changeContent = content
+            }
+            R.id.cf->{
+                //内容 使用cf加密
+                changeContent = " cf $content cf"
+            }
+            R.id.pgp->{
+                //内容 pgp加密
+                changeContent = " pgp $content pgp"
+            }
+            else-> {
+                runOnUiThread {
+                    showToast(getString(R.string.send_method_hint))
+                }
+                return
+            }
+        }
+        try {
+            ts.connect(host, loginUser, loginPwd)
+            //邮件的文本内容
+            message.setText("This is a test mail. $changeContent")
+            ts.sendMessage(message, message.allRecipients)
+            LogUtil.e(TAG!!, "发送成功 $changeContent")
+        } catch (e: MessagingException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun initSendInfo(){
+        Security.addProvider(org.bouncycastle.jce.provider.BouncyCastleProvider())
+        val prop = Properties()
+        prop.setProperty("mail.host", host)
+        prop.setProperty("mail.transport.protocol", "smtp")
+        prop.setProperty("mail.smtp.auth", "true")
+//        prop.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory")
+        //使用JavaMail发送邮件的5个步骤
+        //1、创建session
+        val session = Session.getInstance(prop)
+        //开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
+        session.debug = BuildConfig.DEBUG
+        //2、通过session得到transport对象
+        ts = session.transport
+        //3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
+        //4、创建邮件
+        message = MimeMessage(session)
+        //5、发送邮件
     }
 }
